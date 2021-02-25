da51898d0eed8047e90cc52c5d964a894fa9f7de
Nebury <holasoyluis1998@gmail.com>
2021-02-25 08:17:28 -0600

    Laboratorio #5

--- client/chatroomwidget.cpp
+++ client/chatroomwidget.cpp
@@ -18,7 +18,13 @@
  **************************************************************************/
 
 #include "chatroomwidget.h"
-
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm> 
+#include <sstream> 
+#include <iterator> 
+#include <array>
 #include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QLabel>
 #include <QtWidgets/QToolButton>
@@ -646,8 +652,40 @@ QString ChatRoomWidget::sendCommand(const QStringRef& command,
     return tr("Unknown /command. Use // to send this line literally");
 }
 
-void ChatRoomWidget::sendInput()
-{
+void ChatRoomWidget::sendInput(){
+    std::cout << "-------------Quinto Laboratorio------------" << "\n";
+    std::string text = m_chatEdit->toPlainText().toStdString();
+
+    ray.append(text);
+    ray.append(" ");
+    _mensajes_enviados++;
+
+    size_t tt = ray.size();
+    int palabras = 0;
+        for (int i=1;  i<tt ; i++) {
+        if (ray[i-1] == ' ' && isalpha(ray[i])) {
+            palabras++;}    
+        } 
+
+        if (isalpha(ray[0])) {palabras++;}
+
+    std::cout << "La cantidad de palabras es: " << palabras << "\n"; 
+
+    if (_mensajes_enviados  <= 10) {
+        arr.push_back(text);
+    }
+
+        else {
+        arr.erase(arr.begin());
+        arr.push_back(text);
+         }
+
+    int numeromensajes = 1;
+    for (std::string str: arr) {
+        std::cout << "Mensaje #" << numeromensajes << ": " <<str << "\n";
+        numeromensajes++;
+    }
+
     if (!attachedFileName.isEmpty())
         sendFile();
     else {
